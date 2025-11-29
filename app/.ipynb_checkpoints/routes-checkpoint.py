d6ef40c4ff9e674833edab3d73659d7bcd405109
Farhanabbas1999 <2414414@leedstrinity.ac.uk>
2025-11-29 22:39:01 +0000

    registration and login data insertion

--- app/.ipynb_checkpoints/routes-checkpoint.py
+++ app/.ipynb_checkpoints/routes-checkpoint.py
@@ -1,7 +1,48 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from .models import User, db
 
-bp = Blueprint("main", __name__)
+bp = Blueprint("main", _name_)
 
 @bp.route("/")
 def home():
-    return "Flask app is working !"
+    return "Flask app working successfully!"
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User(username=username)
+        user.set_password(password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash("Registration successful! You can now login.")
+        return redirect(url_for("main.login"))
+
+    return render_template("register.html")
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for("main.home"))
+        else:
+            flash("Invalid username or password")
+
+    return render_template("login.html")
+
+@bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.login"))
--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,34 @@
-from flask import Flask
+rom flask import Flask
 from flask_wtf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 csrf = CSRFProtect()
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = "main.login"
 
 def create_app():
-    app = Flask(__name__)   
-    app.config['SECRET_KEY'] = 'Farhan12'
+    app = Flask(_name_)
+    app.config['SECRET_KEY'] = 'mysecretkey123'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     csrf.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from .routes import bp
     app.register_blueprint(bp)
+
+    with app.app_context():
+        db.create_all()
 
     return app
